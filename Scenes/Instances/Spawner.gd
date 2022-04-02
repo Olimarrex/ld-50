0f907e8d1856d5ef0cd49ec2f810bc9dd8c4565e
Jone Martin Skaara <jone.skaara@lyse.no>
2022-04-02 14:43:27 +0200

    #22: Generalize Spawner to be able to spawn any node from exported parameter

--- Scenes/Instances/Spawner.gd
+++ Scenes/Instances/Spawner.gd
@@ -1,31 +1,25 @@
 extends Node2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+export var pathToNode: String = "res://Scenes/Instances/Gobbo.tscn"
+var nodeToSpawn
 
 
-var gobbo;
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	#print('test');
-	gobbo = preload("res://Scenes/Instances/Gobbo.tscn");
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
+	nodeToSpawn = load(pathToNode);
+	if nodeToSpawn == null:
+		print("Error: Spawner does not have a reference to the node it is set to spawn")
 
 func _on_Timer_timeout():
+	if (nodeToSpawn == null):
+		return
+		
 	var test = get_parent().get_node('Player/KinematicBody2D/Camera2D').get_camera_position();
 	for i in range(1, 12):
 		var x = rand_range(test.x - 1200, test.x + 1200);
 		var y = rand_range(test.y - 800, test.y + 800);
-		var instance = gobbo.instance();
+		var instance = nodeToSpawn.instance();
 		instance.get_node('KinematicBody2D').set_position(Vector2(x, test.y + 800 * (((i % 2) * 2) - 1)));
 		get_parent().add_child(instance);
-		var instance2 = gobbo.instance();
+		var instance2 = nodeToSpawn.instance();
 		instance2.get_node('KinematicBody2D').set_position(Vector2(test.x + 1000 * (((i % 2) * 2) - 1), y));
 		get_parent().add_child(instance2);
