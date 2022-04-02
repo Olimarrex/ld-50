d6e2e94806a549d1d32fc2a4441aacc6724fd01d
algodoogle <ant@codefade.org>
2022-04-02 15:53:41 +0100

    add to clock

---
+++ Scenes/Instances/clock.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+
+signal add_time
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	connect("add_time", get_parent().get_node("CanvasLayer/playerUI") , "_on_Node2D_add_time")
+	pass # Replace with function body.
+
+
+
+func _on_Area2D_pick_up():
+	emit_signal("add_time", 10)
+
