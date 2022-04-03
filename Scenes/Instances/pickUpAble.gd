058ac014f9e1156de435684f8a422cb465e48de6
algodoogle <ant@codefade.org>
2022-04-03 10:32:44 +0100

    test scean

--- Scenes/Instances/pickUpAble.gd
+++ Scenes/Instances/pickUpAble.gd
@@ -12,7 +12,10 @@ func _process(delta):
 		var dir = global_position.direction_to(target.get_position())
 		if global_position.distance_squared_to(target.get_position()) < distanceToPickUpSqared:
 			emit_signal("pick_up")
+			#hide()
+			#set_process(false)
 			get_parent().queue_free()
+			
 		position += transform.x * speed * delta * dir[0]
 		position += transform.y * speed * delta * dir[1]
 
