f3eded7a69ab915a8c84be255f7f8b0b586aa115
Jone Martin Skaara <jone.skaara@lyse.no>
2022-04-04 21:53:52 +0200

    Fix spamming score possible

--- Scenes/Instances/deathScreen.gd
+++ Scenes/Instances/deathScreen.gd
@@ -9,8 +9,9 @@ func restartGame():
 	get_tree().paused = false
 	get_tree().change_scene("res://Scenes/Instances/test.tscn")
 
-
 func _on_submit_pressed():
+	print("Submitting score = " + str(Autoload.gameScore))
+	get_node("HBoxContainer").get_node("VBoxContainer").get_node("HBoxContainer").get_node("acceptButton2").disabled = true
 	var score_id = yield(SilentWolf.Scores.persist_score(get_node("HBoxContainer/VBoxContainer/HBoxContainer/LineEdit").text, Autoload.gameScore), "sw_score_posted")
 	if score_id:
 		get_tree().change_scene("res://Scenes/tests/scoreBord.tscn")
