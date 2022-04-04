8821ecebf84ef4a8e28636aa9a92ea2504f1f381
algodoogle <ant@codefade.org>
2022-04-04 14:57:49 +0100

    a bit more balance

---
+++ Scenes/Instances/Eyeo.gd
@@ -0,0 +1,59 @@
+extends KinematicBody2D
+
+export (int) var speed = 200
+export (int) var damage = 1
+export (int) var maxHealth = 100
+export (int) var pickUpTime = 1
+export (float) var knockbackRes = 1.0
+export (NodePath) var spright = null
+
+var dir = Vector2(1,0)
+
+var velocity = Vector2()
+var health;
+var pickupScene
+
+func _ready():
+	pickupScene = load("res://Scenes/Instances/clock.tscn").instance()
+	health = maxHealth;
+	get_node("enemyHealthBar").max_value = maxHealth;
+	get_node("enemyHealthBar").value = maxHealth;
+	if spright == null:
+		print("Error: Enemy with name '" + get_parent().name + "' is missing reference to its sprite")
+
+func setPower(powerIn):
+	speed *= powerIn
+	damage *= powerIn
+	maxHealth *= powerIn
+	set_modulate(Color(1,1.1-(powerIn/5.0),1.1-(powerIn/5.0)))
+
+func _process(_delta):
+	velocity += ((dir * speed) / 10)
+	velocity /= 1.05
+	velocity = move_and_slide(velocity)
+	for i in get_slide_count():
+		var collision = get_slide_collision(i)
+		if collision.collider.is_in_group("player"):
+			inflictDamage(collision)
+	get_node(spright).set_flip_h(dir[0] < 0);
+
+func inflictDamage(_entity):
+	get_parent().get_parent().get_node("Player/KinematicBody2D").takeDamage(damage)
+
+func takeDamage(dmg):
+	health -= dmg;
+	var playerUI = get_tree().get_current_scene().get_node("CanvasLayer/playerUI")
+	playerUI.get_node('SoundHitHurt').pitch_scale = rand_range(0.8, 1.2);
+	playerUI.get_node('SoundHitHurt').play();
+	$enemyHealthBar.value = health
+	if health <= 0:
+		call_deferred("die")
+
+func knockback(dir):
+	velocity += dir * knockbackRes;
+
+func die():
+	pickupScene.setTime(pickUpTime)
+	pickupScene.get_node("Area2D").set_position(position + get_parent().position)
+	get_parent().get_parent().add_child(pickupScene)
+	get_parent().free()
--- Scenes/Instances/Spawner.gd
+++ Scenes/Instances/Spawner.gd
@@ -3,6 +3,7 @@ extends Node2D
 export (int) var maxAllowedEnemies = 1000
 export var currentWave = 0;
 export var enemyNodes = {
+	"eyeo": "res://Scenes/Instances/Eyeo.tscn",
 	"gobbo": "res://Scenes/Instances/Gobbo.tscn",
 	"ghost": "res://Scenes/Instances/Ghost.tscn",
 	"zombo": "res://Scenes/Instances/Zombie.tscn",
@@ -16,118 +17,121 @@ var waves = [
 		"gobbo": 2,
 		"ghost": 4,
 		"zombo": 1,
-		"time": 5
+		"time": 100
 	},
-#	{
-#		"gobbo": 2,
-#		"ghost": 4,
-#		"time": 10
-#	},
-#	{
-#		"gobbo": 4,
-#		"ghost": 6,
-#		"time": 15
-#	},
-#	{
-#		"gobbo": 0,
-#		"ghost": 10,
-#		"time": 15
-#	},
-#	{
-#		"gobbo": 0,
-#		"ghost": 15,
-#		"zombo": 1,
-#		"time": 15
-#	},
-#	{
-#		"gobbo": 10,
-#		"ghost": 0,
-#		"zombo": 5,
-#		"time": 20
-#	},
-#	{
-#		"gobbo": 3,
-#		"ghost": 3,
-#		"zombo": 15,
-#		"time": 20
-#	},
-#	{
-#		"gobbo": 4,
-#		"ghost": 4,
-#		"zombo": 16,
-#		"time": 20
-#	},
-#	{
-#		"gobbo": 5,
-#		"ghost": 5,
-#		"zombo": 18,
-#		"time": 20
-#	},
-#	{
-#		"gobbo": 20,
-#		"ghost": 20,
-#		"zombo": 5,
-#		"time": 20
-#	},
-#	{
-#		"gobbo": 40,
-#		"ghost": 0,
-#		"time": 25
-#	},
-#	{
-#		"gobbo": 0,
-#		"ghost": 0,
-#		"zombo": 30,
-#		"time": 25
-#	},
-#	{
-#		"gobbo": 10,
-#		"ghost": 0,
-#		"zombo": 30,
-#		"time": 20
-#	},
-#	{
-#		"gobbo": 30,
-#		"ghost": 30,
-#		"zombo": 30,
-#		"time": 20
-#	},
-#	{
-#		"gobbo": 10,
-#		"ghost": 50,
-#		"zombo": 10,
-#		"time": 20
-#	},
-#	{
-#		"gobbo": 100,
-#		"ghost": 0,
-#		"zombo": 0,
-#		"time": 25
-#	},
-#	{
-#		"gobbo": 80,
-#		"ghost": 0,
-#		"zombo": 80,
-#		"time": 25
-#	},
-#	{
-#		"gobbo": 50,
-#		"ghost": 40,
-#		"zombo": 60,
-#		"time": 25
-#	},
-#	{
-#		"gobbo": 90,
-#		"ghost": 30,
-#		"zombo": 80,
-#		"time": 30
-#	},
-#	{
-#		"gobbo": 100,
-#		"ghost": 100,
-#		"zombo": 100,
-#		"time": 30
-#	},
+	{
+		"gobbo": 2,
+		"ghost": 4,
+		"time": 10
+	},
+	{
+		"gobbo": 4,
+		"ghost": 6,
+		"time": 15
+	},
+	{
+		"gobbo": 0,
+		"ghost": 10,
+		"time": 15
+	},
+	{
+		"gobbo": 0,
+		"ghost": 15,
+		"zombo": 1,
+		"time": 15
+	},
+	{
+		"gobbo": 10,
+		"ghost": 0,
+		"zombo": 5,
+		"time": 20
+	},
+	{
+		"gobbo": 3,
+		"ghost": 3,
+		"zombo": 15,
+		"time": 20
+	},
+	{
+		"gobbo": 4,
+		"ghost": 4,
+		"zombo": 16,
+		"time": 20
+	},
+	{
+		"gobbo": 5,
+		"ghost": 5,
+		"zombo": 18,
+		"time": 20
+	},
+	{
+		"gobbo": 20,
+		"ghost": 20,
+		"zombo": 5,
+		"eyeo": 25,
+		"time": 20
+	},
+	{
+		"gobbo": 40,
+		"ghost": 0,
+		"time": 25
+	},
+	{
+		"gobbo": 0,
+		"ghost": 0,
+		"zombo": 30,
+		"time": 25
+	},
+	{
+		"gobbo": 10,
+		"ghost": 0,
+		"zombo": 30,
+		"time": 20
+	},
+	{
+		"gobbo": 30,
+		"ghost": 30,
+		"zombo": 30,
+		"time": 20
+	},
+	{
+		"gobbo": 10,
+		"ghost": 50,
+		"zombo": 10,
+		"eyeo": 55,
+		"time": 20
+	},
+	{
+		"gobbo": 100,
+		"ghost": 0,
+		"zombo": 0,
+		"time": 25
+	},
+	{
+		"gobbo": 80,
+		"ghost": 0,
+		"zombo": 80,
+		"time": 25
+	},
+	{
+		"gobbo": 50,
+		"ghost": 40,
+		"zombo": 60,
+		"time": 25
+	},
+	{
+		"gobbo": 90,
+		"ghost": 30,
+		"zombo": 80,
+		"eyeo": 100,
+		"time": 30
+	},
+	{
+		"gobbo": 100,
+		"ghost": 100,
+		"zombo": 100,
+		"time": 30
+	},
 	{
 		"countbanks": 1,
 		"gobbo": 100,
@@ -186,7 +190,7 @@ func startNextWave():
 		wave = waves[waves.size() - 1];
 		power = currentWave - waves.size()
 		
-	if "countbanks" in wave:
+	if "countbanks" in wave: 
 		self.get_parent().get_node("CanvasLayer/playerUI/bossHealthBar").show()
 		self.get_parent().get_node("CanvasLayer/playerUI/bossHealthBar/bossName").text = "COUNT BANKS"
 		self.get_parent().get_node("CanvasLayer/playerUI/Shop/startingMusic").stop()
@@ -212,9 +216,24 @@ func startNextWave():
 func spawn(key, count):
 	var cameraPos = get_parent().get_node('Player/KinematicBody2D/Camera2D').get_camera_position();
 	var nodeToSpawn = loadedEnemies[key];
-	for _i in range(0, count):
-		var rand = deg2rad(rand_range(0, 360));
-		var instance = nodeToSpawn.instance();
-		instance.get_node('KinematicBody2D').set_position(cameraPos + (Vector2(1, 0).rotated(rand) * rand_range(1000, 1200)));
-		instance.get_node('KinematicBody2D').setPower(power)
-		get_parent().add_child(instance);
+	
+	if 	key == "eyeo":
+		var randRot = deg2rad(rand_range(0, 360));
+		var randDis = rand_range(1000, 1200)
+
+
+		for i in range(0, count):
+			print("asdasd")
+			var instance = nodeToSpawn.instance();
+			instance.get_node('KinematicBody2D').set_position(Vector2(0, (count/2-i)*50).rotated(randRot) + cameraPos + (Vector2(1, 0).rotated(randRot) * randDis));
+			instance.get_node('KinematicBody2D').dir = Vector2(1, 0).rotated(randRot+deg2rad(180))
+			instance.get_node('KinematicBody2D').setPower(power)
+			get_parent().add_child(instance);
+
+	else:
+		for _i in range(0, count):
+			var rand = deg2rad(rand_range(0, 360));
+			var instance = nodeToSpawn.instance();
+			instance.get_node('KinematicBody2D').set_position(cameraPos + (Vector2(1, 0).rotated(rand) * rand_range(1000, 1200)));
+			instance.get_node('KinematicBody2D').setPower(power)
+			get_parent().add_child(instance);
